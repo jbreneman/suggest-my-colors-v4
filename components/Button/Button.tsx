0765a1df700c77185f4c4746844fd9533cc836cf
Jesse Breneman <talk@jessebreneman.com>
2021-09-27 22:02:34 -0400

    initial commit

---
+++ components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+import styles from './Button.module.css'
+
+const Button = ({ children, color }: {
+    children: ReactNode;
+    color?: string;
+}) => {
+    return (
+        <button style={{ color }}>
+            {children}
+        </button>
+    )
+}
+  
+  export default Button
